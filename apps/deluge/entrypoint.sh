4daa90885f27b8a6029d011af4d28f422f674cec
Devin Buhl <onedr0p@users.noreply.github.com>
2025-04-14 10:11:34 -0400

    feat(deluge): allow port customizations (#347)

--- apps/deluge/entrypoint.sh
+++ apps/deluge/entrypoint.sh
@@ -3,13 +3,23 @@
 
 if [[ ! -f /config/core.conf ]]; then
     cp /defaults/core.conf /config/core.conf
+    sed -i -e "s/58846/${DELUGE_DAEMON_PORT:-58846}/" /config/core.conf
 fi
 
 mkdir -p /config/plugins/.python-eggs
 
-exec \
-    ${DELUGE_BIN:-deluged} \
-    --do-not-daemonize \
-    --config /config \
-    --loglevel ${DELUGE_LOGLEVEL:-info} \
-    "$@"
+OPTS=(
+    "--do-not-daemonize"
+    "--config" "/config"
+    "--loglevel" "${DELUGE_LOGLEVEL:-info}"
+    "--interface" "0.0.0.0"
+)
+
+if [[ ${DELUGE_BIN:-deluged} == "deluged" ]]; then
+    OPTS+=("--ui-interface" "0.0.0.0")
+    OPTS+=("--port" "${DELUGE_DAEMON_PORT:-58846}")
+elif [[ ${DELUGE_BIN:-deluged} == "deluge-web" ]]; then
+    OPTS+=("--port" "${DELUGE_WEB_PORT:-8112}")
+fi
+
+exec ${DELUGE_BIN:-deluged} "${OPTS[@]}" "$@"
